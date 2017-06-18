da52dd4e9dfd629737eb09b3c89775e73fe05616
abby0100 <yuan.xiong@intel.com>
2017-06-18 23:22:30 +0800

    First commit for unity hello world and Button. (Useful for android)

---
+++ Assets/01Scripts/ButtonClick.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonClick : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ClickTest() {
+		Debug.Log ("Button click test");
+	}
+}
---
+++ Assets/01Scripts/HelloWorld.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		Debug.Log ("Update");
+	}
+
+	void OnGUI() {
+//		Debug.Log ("OnGUI");
+
+		GUI.skin.label.fontSize = 40;
+		GUI.Label (new Rect(10, 10, Screen.width, Screen.height), "Hello Unity World xy!");
+
+		if (GUI.Button(new Rect(0, 10, 100, 40), "xyjoin")) {
+			print ("Button print");
+		}
+	}
+}
