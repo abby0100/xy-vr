d29b1bd0b77c349e1cb8e7a113f2497890bc149a
Xiong <yuan.xiong@intel.com>
2017-07-05 17:09:19 +0800

    First commit: add video player on Cube, Sphere and Quad.

---
+++ Assets/MoviePlayer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MoviePlayer : MonoBehaviour
+{
+
+	public Image image;
+	public MovieTexture movie;
+	public AudioSource audioPlayer;
+
+	void Awake()
+	{
+		image.material.mainTexture = movie;
+		Play();
+	}
+
+	public void Play()
+	{
+		movie.Play();
+		audioPlayer.Play();
+	}
+
+	public void Stop()
+	{
+		movie.Stop();
+		audioPlayer.Stop();
+	}
+
+	public void Pause()
+	{
+		movie.Pause();
+		audioPlayer.Pause();
+	}
+}
