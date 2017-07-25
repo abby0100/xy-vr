90bd530d6a66e48eae4d27ab1e2e04efb06557ef
Xiong <yuan.xiong@intel.com>
2017-07-25 17:43:08 +0800

    First commit: add dll source code.

---
+++ Assets/02Scripts/UnityTestDll.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class UnityTestDll : MonoBehaviour {
+
+	[DllImport("TestDll")]
+	private static extern int add (int x, int y);
+
+	int i = add(5, 7);
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnGUI() {
+		GUI.Button (new Rect(1, 1, 200, 100), "this dll i = 5+7, i is " + i);
+	}
+}
