3577f9efe3da43959f4373deabacfb583e90259e
Xiong <yuan.xiong@intel.com>
2017-08-01 11:15:51 +0800

    add Ogg video file play script.

--- Assets/MoviePlayer.cs
+++ Assets/MoviePlayer.cs
@@ -1,34 +1,60 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;  
+using System.Collections;  
+using UnityEngine.Audio;  
+using UnityEngine.UI;  
+public class MoviePlayer : MonoBehaviour {  
+	public MovieTexture movieTexture;  
 
-public class MoviePlayer : MonoBehaviour
-{
+	// Use this for initialization  
+	void Start () {  
+		
+		StartCoroutine(DownloadMovie()); 
 
-	public Image image;
-	public MovieTexture movie;
-	public AudioSource audioPlayer;
+		movieTexture.Play();  
+	}    
 
-	void Awake()
+	IEnumerator DownloadMovie()  
 	{
-		image.material.mainTexture = movie;
-		Play();
-	}
 
-	public void Play()
-	{
-		movie.Play();
-		audioPlayer.Play();
-	}
+		//WWW www = new WWW("file:///D://Project//Movie/Data/Movie//movie.ogv");  
+//		movietexture，只能播放OGG和OVG，而且质量比较低．如果要转成高质量的视频，文件尺寸比较大．　放弃，继续找．
+//		WWW www = new WWW("file:///D://test/itflexires.mp4");
+		WWW www = new WWW("file:///D://test/movie.ogg");
 
-	public void Stop()
-	{
-		movie.Stop();
-		audioPlayer.Stop();
-	}
+		Debug.Log(Time.time);  
+		movieTexture = www.GetMovieTexture();  
 
-	public void Pause()
-	{
-		movie.Pause();
-		audioPlayer.Pause();
-	}
-}
+		Debug.Log ("DownloadMovie movieTexture is null ? " + (movieTexture.ToString()));
+		GetComponent<Renderer>().material.mainTexture = movieTexture;  
+		movieTexture.loop = true; 
+		yield return www; 
+	}  
+		
+
+	void OnGUI()  
+	{  
+		if (GUILayout.Button("播放/继续"))  
+		{  
+			//播放/继续播放视频      
+			if (!movieTexture.isPlaying)  
+			{  
+				movieTexture.Play();  
+				// audio.Play();  
+			}  
+		}  
+
+		if (GUILayout.Button("暂停播放"))  
+		{  
+			//暂停播放      
+			movieTexture.Pause();  
+			// audio.Pause();  
+		}  
+
+		if (GUILayout.Button("停止播放"))  
+		{  
+			//停止播放      
+			movieTexture.Stop();  
+			// audio.Stop();  
+		}  
+	}  
+}  
